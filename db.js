f5ddfe40413ef1889c0b8aff58a78ec3ac45f0b5
MoMohamoud <moeddie@hotmail.com>
2015-10-13 22:24:47 -0700

    add mongoLab hosted DB

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 module.exports = {
 	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
-	'url' : 'mongodb://localhost/test'
+	 //'url' : 'mongodb://localhost/test'
+	'url' : 'mongodb://spnNetwork:reerka@ds041150.mongolab.com:41150/reerka'
 }
